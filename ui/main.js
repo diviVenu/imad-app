c2135ea49da62b782aec02b5933d37fa89a9c4f4
diviVenu <30758028+diviVenu@users.noreply.github.com>
2017-08-20 19:43:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
 console.log('Loaded!');
+var button=document.getelementbyid("counter");
+button.onclick=function()
+{
+    var req=new xmlhttprequest();
+    req.onreadystatechange=function()
+    {
+        if(req.readystate===xmlhttprequest.Done)
+        {
+            if(req.status===200)
+            {
+                var counter=req.responsetext;
+                var span=document.getelementbyid("count");
+                span.innerHTML=counter.toString();
+                            }
+        }
+    };
+    req.open('GET','http://divya09feb91.imad.hasura-app.io',true);
+    req.send(null);
+};
