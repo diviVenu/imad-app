bae3b18a71c45124505c1fba88dff2846c776b92
diviVenu <30758028+diviVenu@users.noreply.github.com>
2017-08-20 20:10:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,16 @@
-var button=document.getElementById('counter');
-button.onclick=function ()
+var button = document.getElementById('counter');
+button.onclick = function ()
 {
-    var request=new XMLHttpRequest();
+    var request = new XMLHttpRequest();
     request.onreadystatechange=function ()
     {
-        if(request.readystate===XMLHttpRequest.Done)
+        if(request.readyState === XMLHttpRequest.Done)
         {
-            if(request.status===200)
+            if(request.status === 200)
             {
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
                             }
         }
     };
