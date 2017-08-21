fba2c2ccce780a7fe5998450d8c4d4f35a515f8c
diviVenu <30758028+diviVenu@users.noreply.github.com>
2017-08-21 16:09:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,9 @@
 var button = document.getElementById('counter');
+var counter=0;
 button.onclick = function ()
 {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function ()
-    {
-        if(request.readyState == XMLHttpRequest.Done)
-        {
-            if(request.status === 200)
-            {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
+    counter=counter+1;
+     var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
-                document.getElementById('counter').value=counter.toString();
-                
-                                           }
-        }
-    };
-    request.open('GET','http://divya09feb91.imad.hasura-app.io/counter',true);
-    request.send(null);
+   
 };
