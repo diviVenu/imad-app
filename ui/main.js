152e8e1deab2c3b9090cc8541b476b48a048948f
diviVenu <30758028+diviVenu@users.noreply.github.com>
2017-08-22 14:31:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,25 +7,25 @@
     // var span = document.getElementById('count');
               //  span.innerHTML = counter.toString();
   // };
-function b_click() {
-     
-      var request = new XMLHttpRequest();
-      
-    request.onreadystatechange = function ()
+function b_click(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() 
     {
-        if(request.readyState === XMLHttpRequest.Done)
-        {
-            if(request.status === 200)
-            {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    request.open('GET', 'http://divya09feb91.imad.hasura-app.io/counter', true);
-    request.send(null);
- } 
+          if (request.readyState === XMLHttpRequest.DONE) 
+          {
+              //Take Some Action
+              if (request.status === 200) 
+                { 
+                    var counter = request.responseText; 
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                } 
+          } 
+    }; 
+    request.open("GET", "http://divya09feb91.imad.hasura-app.io/counter", true);
+    request.send(); 
+    console.log('EXECUTED');
+}
   
 /* first try to get the names on button click
 var nameInput= document.getElementById('name');
