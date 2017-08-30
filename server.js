9a8e7bfb46d2dbf7ecee76c04c174a43aabbe656
diviVenu <30758028+diviVenu@users.noreply.github.com>
2017-08-30 17:09:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.get('/submit-name/:name', function (req, res) {
 var pool= new Pool(config);
 
 app.get('/test-db',function (req, res) {
-   pool.query('select * from Test', function(err, result)
+   pool.query('SELECT * FROM Test', function(err, result)
    {
    if (err) {
             res.status(500).send(err.toString());
