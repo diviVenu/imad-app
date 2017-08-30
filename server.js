21fd8987ae4e4ee659ef24dfaa4d2a3b58288fc9
diviVenu <30758028+diviVenu@users.noreply.github.com>
2017-08-30 14:56:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ var articles={
 
 //Begin Module P10
 //test-db
-var pool= new pool(config);
+var pool= new Pool(config);
 
 app.get('/test-db',function (req, res) {
    pool.query('select * from test', function(err, result)
