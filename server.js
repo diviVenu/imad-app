5b464c72fcead9956d9ba2939e0e2f12da7f031a
diviVenu <30758028+diviVenu@users.noreply.github.com>
2017-09-10 21:05:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -277,8 +277,7 @@ app.post('/create-user', function(req,res){
                         
                         if(hashedPassword===dbString)
                         {
-                             req.session.auth={userId: result.rows[0].id};
-                            res.send('Credentials Correct');
+                                                        res.send('Credentials Correct');
                         }
                         else
                         {
