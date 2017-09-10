13400334196020be39812cc043fb54f5d34762d5
diviVenu <30758028+diviVenu@users.noreply.github.com>
2017-09-10 19:52:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.get('/submit-name/:name', function (req, res) {
 var pool= new Pool(config);
 
 app.get('/test-db',function (req, res) {
-   pool.query('SELECT * FROM USERS', function(err, result)
+   pool.query('SELECT * FROM users_a', function(err, result)
    {
    if (err) {
             res.status(500).send(err.toString());
